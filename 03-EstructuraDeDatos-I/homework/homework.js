2f6c5939a20289fbf97489c605b5242da7997d46
Santiago Caro <santiagocaroumbariba12@gmail.com>
2022-04-28 17:10:20 -0500

    finish homework03

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // Las funciones nFactoria y nFibonacci deben resolverlas
 // usando recursión. Una vez realizadas de esa forma pueden probar hacerlas
 // de forma iterativa pero esto último no es obligatorio.
@@ -7,26 +7,53 @@ function nFactorial(n) {
   // devolvé el factorial de n (n!)
   // ej:
   // el factorial de 3 es 6 (3 * 2 * 1)
+  if (n <= 0) {
+    return "Los numeros negativos o igual a cero no tienen factorial";
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 }
 
 function nFibonacci(n) {
+  // 0  1  2  3  4  5  6  7   8   9   10  11  12
   // Secuencia de Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,…
   // Retorna el enésimo numero de la serie
   // nFibonacci(0) // 0  // el elemento 0 es cero
   // nFibonacci(1) // 1 // el elemento 1 es 1
   // nFibonacci(6) // 1 // el elemento 6 es 8
-
+  if (n <= 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 }
 
 // Para esta parte no es necesario utilizar recursión.
 // Implementa la clase Queue que debe contener los siguientes métodos:
 // enqueue: Agrega un valor a la queue. Respeta el orden existente.
-// dequeue: Remueve un valor de la queue. Obedece a FIFO y respeta el underflow (devuelve undefined cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
+// dequeue: Remueve un valor de la queue. Obedece a FIFO y respeta el underflow
+// (devuelve undefined cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
 // size: Devuelve el número de elementos que contiene la queue.
 
 function Queue() {
-
-}
+    this.queue = [];
+    this.enqueue = function (x) {
+      return this.queue.push(x);
+    };
+    this.dequeue = function (y) {
+      return this.queue.shift(y);
+    };
+    this.size = function () {
+      if (this.queue.length === 0) {
+        return 0;
+      }
+      return this.queue.length;
+    };
+  };
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
@@ -34,5 +61,5 @@ function Queue() {
 module.exports = {
   Queue,
   nFactorial,
-  nFibonacci
+  nFibonacci,
 };
