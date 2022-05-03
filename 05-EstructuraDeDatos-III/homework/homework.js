206d4fc538fe8ac5ad91abef60bcb18ab04566df
Santiago Caro <santiagocaroumbariba12@gmail.com>
2022-05-03 14:39:43 -0500

    finish Homework DataStructure

--- 05-EstructuraDeDatos-III/homework/homework.js
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // resolve estos ejercicios usando recursión
 
 // Binary Seach Tree
@@ -8,13 +8,104 @@
 // corre depth-first (en recorrido "post-order" cuando depthFirstForEach() es ejecutado con la opcion "post-order"
 // corre breadth-first cuando breadthFirstForEach() es ejecutado
 // Observar imagen de la carpeta "homework" llamada "bst.png". Allí encontraran dibujado el arbol utilizado para los tests
-function BinarySearchTree() {
-
+function BinarySearchTree(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+  this._length = 1;
 }
+BinarySearchTree.prototype.insert = function (x) {
+  if (x === this.value) {
+    return
+  }
+  var current = new BinarySearchTree(x);
+  if (x > this.value) {
+    if (this.right == null) {
+      this._length++
+      this.right = current;
+    } else {
+      this.right.insert(x);
+    }
+  } else {
+    if (this.left == null) {
+      this._length++
+      this.left = current;
+    } else {
+      this.left.insert(x);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function (google) {
+  if (google === this.value) {
+    return true;
+  }
+  if (google > this.value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(google);
+    }
+  } else {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(google);
+    }
+  }
+};
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, opcion) {
+  if (!opcion || opcion === "in-order") { 
+    if (this.left) { 
+      this.left.depthFirstForEach(cb, opcion);
+    }
+    cb(this.value); 
+    if (this.right) { 
+      this.right.depthFirstForEach(cb, opcion);
+    }
+  } else if (opcion === "pre-order") { 
+    cb(this.value); 
+    if (this.left) { 
+      this.left.depthFirstForEach(cb, opcion);
+    }
+    if (this.right) { 
+      this.right.depthFirstForEach(cb, opcion);
+    }
+  } else if (opcion === "post-order") { 
+    if (this.left) { 
+      this.left.depthFirstForEach(cb, opcion);
+    }
+    if (this.right) { 
+      this.right.depthFirstForEach(cb, opcion);
+    }
+    cb(this.value); 
+  }
+};
+
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, array) {
+    if (!array) {
+    var array = [];
+  }
+  if (this.left) {
+    array.push(this.left); 
+  }
+  if (this.right) {
+    array.push(this.right);
+  }
+  cb(this.value);
+  if (array.length > 0) {
+    array.shift().breadthFirstForEach(cb, array);
+  }
+};
+
+BinarySearchTree.prototype.size = function () {
+  return this._length
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
